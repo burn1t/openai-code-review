8e9413e2c75bae9591b1d8d53fd26ec87c7ab478
F1oyd <floydcode@163.com>
2026-03-05 18:01:59 +0800

    docs: 注释

--- openai-code-review-sdk/src/main/java/com/floyd/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/floyd/sdk/OpenAiCodeReview.java
@@ -10,8 +10,9 @@ public class OpenAiCodeReview {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("测试执行");
 
-        // 1.代码检出
+        // 1.代码检出：基于 Github action 环境，在 Java 程序中调用 Git 命令，获取当前仓库最近一次提交的代码变更内容
         ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        // 设置工作目录为当前项目根目录，告诉 Git 命令在哪个文件夹下执行
         ProcessBuilder directory = processBuilder.directory(new File("."));
 
         Process process = directory.start();
